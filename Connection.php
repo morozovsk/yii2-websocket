2770f7ce762f037a7e6c05bf4ea860d732c4d47e
morozovsk <morozovsk@gmail.com>
2015-09-08 23:14:04 +0300

    add method send in component

--- Connection.php
+++ Connection.php
@@ -7,4 +7,21 @@ use yii\base\Component;
 class Connection extends Component
 {
     public $servers;
+
+    protected $_instances = [];
+
+    public function getInstance($server) {
+        if (!isset($this->_instances[$server])) {
+            $this->_instances[$server] = stream_socket_client ($this->servers[$server]['localsocket'], $errno, $errstr);//соединямся с мастер-процессом:
+        }
+        return $this->_instances[$server];
+    }
+
+    public function send($message, $server = null) {
+        if (!$server) {
+            reset($this->servers);
+            $server = key($this->servers);
+        }
+        return fwrite($this->getInstance($server), $message . "\n");
+    }
 }
